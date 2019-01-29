592f99ae76d7886abec1fb8facc2acc69fa2fb64
Bojoofu <graujsheridan@gmail.com>
2019-01-28 19:58:00 -0500

    Initial Commit

---
+++ ios_assignment_1_James_Grau/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_assignment_1_James_Grau
+//
+//  Created by Xcode User on 2019-01-27.
+//  Copyright © 2019 James Grau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
